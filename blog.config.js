7d6eaf5291aa4f00754ffd72b05736cef829f298
xulele <527190972@qq.com>
2023-05-20 09:52:11 +0800

    add ads

--- blog.config.js
+++ blog.config.js
@@ -279,7 +279,7 @@ const BLOG = {
   // <---- 站点统计
 
   // 谷歌广告
-  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || '', // 谷歌广告ID e.g ca-pub-xxxxxxxxxxxxxxxx ca-pub-4717143457963822
+  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || 'ca-pub-4717143457963822', // 谷歌广告ID e.g ca-pub-xxxxxxxxxxxxxxxx ca-pub-4717143457963822
 
   // 自定义配置notion数据库字段名
   NOTION_PROPERTY_NAME: {
